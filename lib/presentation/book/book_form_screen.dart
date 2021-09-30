b1109d339d019e6ea0fb104ea41681614418f71c
Ahmad Adil <=>
2021-09-30 15:49:37 +0700

    try to make form

--- lib/presentation/book/book_form_screen.dart
+++ lib/presentation/book/book_form_screen.dart
@@ -12,27 +12,62 @@ class BookFormScreen extends StatefulWidget {
 }
 
 class _BookFormScreenState extends State<BookFormScreen> {
+  TextEditingController title = new TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Add Book'),),
       body: Container(
-        child: TextButton(
-            onPressed: (){
-              setState(() {
-                widget.bookViewModel.addBook(Book(
-                  title: 'Belaja Googld Cloud',
-                  description: 'Description',
-                  year: 2021,
-                  pages: 50,
-                  language: 'Indonesia',
-                  publisher: 'Legionare',
-                  price: 300000,
-                  stock: 8
-                ));
-              });
-            }, 
-            child: Text('Submit code')),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: title,
+                  decoration: InputDecoration(hintText: 'Enter Book title'),
+                  validator:(value) {
+                    if (value == null || value.isEmpty){
+                      return 'Please enter!';
+                    } else {
+                      return null;
+                    }
+                  }
+              ),
+              TextFormField(
+                // controller: description,
+                  decoration: InputDecoration(hintText: 'Enter The Description'),
+                  validator:(value) {
+                    if (value == null || value.isEmpty){
+                      return 'Please enter!';
+                    } else {
+                      return null;
+                    }
+                  }
+              ),
+              TextButton(
+                  onPressed: (){
+                    setState(() {
+                      widget.bookViewModel.addBook(Book(
+                          // title: 'Belaja Googld Cloud',
+                          title: title.text,
+                          description: 'Description',
+                          year: 2021,
+                          pages: 50,
+                          language: 'Indonesia',
+                          publisher: 'Legionare',
+                          price: 300000,
+                          stock: 8
+                      ));
+                    });
+                  },
+                  child: Text('Submit code')
+              ),
+            ]
+          )
+        )
+
       ),
     );
   }
